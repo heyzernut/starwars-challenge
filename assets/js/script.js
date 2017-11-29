3f7c6984f32fd41a5a72054af1add91a0d2a1e3d
heyzernut <hazeltoh@Hazels-MacBook-Pro.local>
2017-11-29 19:45:42 +0800

    update image png

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,37 +11,37 @@ axios.get(`https://swapi.co/api/films/7/`)
       randomChar: randomChar,
       images: [
         { attr: 'https://swapi.co/api/people/1/',
-          image: './assets/images/swchar/manss.png'
+          image: './assets/images/swchar/luke-skywalker.png'
         },
         { attr: 'https://swapi.co/api/people/3/',
           image: './assets/images/swchar/R2-D2.png'
         },
         { attr: 'https://swapi.co/api/people/5/',
-          image: './assets/images/swchar/Leia-Organa.png'
+          image: './assets/images/swchar/leia-organa.png'
         },
         { attr: 'https://swapi.co/api/people/13/',
-          image: './assets/images/swchar/manss.png'
+          image: './assets/images/swchar/chewbacca.png'
         },
         { attr: 'https://swapi.co/api/people/14/',
-          image: './assets/images/swchar/manss.png'
+          image: './assets/images/swchar/han-solo.png'
         },
         { attr: 'https://swapi.co/api/people/27/',
-          image: './assets/images/swchar/manss.png'
+          image: './assets/images/swchar/ackbar.png'
         },
         { attr: 'https://swapi.co/api/people/84/',
-          image: './assets/images/swchar/manss.png'
+          image: './assets/images/swchar/finn2.png'
         },
         { attr: 'https://swapi.co/api/people/85/',
-          image: './assets/images/swchar/manss.png'
+          image: './assets/images/swchar/reys.png'
         },
         { attr: 'https://swapi.co/api/people/86/',
-          image: './assets/images/swchar/manss.png'
+          image: './assets/images/swchar/poe-dameron.png'
         },
         { attr: 'https://swapi.co/api/people/87/',
-          image: './assets/images/swchar/manss.png'
+          image: './assets/images/swchar/BB8.png'
         },
         { attr: 'https://swapi.co/api/people/88/',
-          image: './assets/images/swchar/owen_lars.png'
+          image: './assets/images/swchar/captain-phasma.png'
         }
       ]
     },
